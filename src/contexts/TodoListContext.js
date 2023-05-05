63ea708168f9cf00228ed4f73fb110dc425045fc
Elanis <elanis@hotmail.com>
2023-05-05 15:49:24 +0200

    Persistence

--- src/contexts/TodoListContext.js
+++ src/contexts/TodoListContext.js
@@ -1,13 +1,28 @@
-import { useState, createContext } from 'react';
+import { useEffect, useState, createContext } from 'react';
 
 export const TodoListContext = createContext();
 
+const LOCALSTORAGE_KEY = 'LIST_STORAGE';
+
 export default function TodoListContextProvider({ children }) {
-	const [list, setList] = useState([
-		'Un',
-		'Deux',
-		'Trois'
-	]);
+	const localStorageData = localStorage.getItem(LOCALSTORAGE_KEY);
+	let defaultData = [];
+	if(localStorageData) {
+		const data = JSON.parse(localStorageData);
+
+		if(Array.isArray(data)) {
+			defaultData = data;
+		}
+	}
+
+	const [list, setList] = useState(defaultData);
+
+	useEffect(() => {
+		localStorage.setItem(
+			LOCALSTORAGE_KEY,
+			JSON.stringify(list)
+		);
+	}, [list]);
 
 	return (
 		<TodoListContext.Provider value={{list, setList}}>
